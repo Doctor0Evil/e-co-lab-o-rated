f18fd3950c0ba326b4fd7fa9f1d8640fe9ebf31e
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-01-27 03:39:36 -0700

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export * from "./domain/materials";
+export * from "./domain/decay";
+export * from "./domain/lca";
+export * from "./domain/telemetry";
+export * from "./domain/proofs";
+
+export * from "./config/phoenix.config";
+
+export * from "./services/ecoScoreService";
+export * from "./services/decayService";
+export * from "./services/displacementService";
+export * from "./services/pfasLibraryService";
+
+export * from "./adapters/telemetryShardAdapter";
